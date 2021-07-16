6a341dc81177fadf76d79fc76ee5960ca58ae949
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-07-16 15:30:27 -0300

    create ex002

---
+++ Exercicios resolvidos Pascal/Conjuntos/ex002.pas
@@ -0,0 +1,64 @@
+
+program ex039;
+uses CRT;
+const
+    max = 255;
+type
+    inteiro = set of 2..max;
+var
+    val_primos, numeros : inteiro;
+    i, num: integer;
+
+function testa_primo(num : integer) : inteiro;
+var
+    i, j : integer;
+    primo : boolean;
+    primos : inteiro;
+begin
+    primos := [];
+    primo := true;
+    for i := 2 to num do
+    begin
+        for j := 2 to (i - 1) do
+            if (i mod j) = 0 then
+                primo := false;
+        if primo then
+            primos += [i]
+        else
+            primo := true;
+    end;
+    testa_primo := primos;
+end;
+
+function testa_primo2(num : integer) : inteiro;
+var
+    numeros, primos : inteiro;
+    i, j : integer;
+begin
+    numeros := [2..num];
+    primos := numeros;
+    for i := num downto 2 do
+        for j := 2 to i - 1 do
+            if (i mod j = 0) and (i <> j) then
+                primos -= [i];
+    testa_primo2 := primos;
+end;
+
+begin
+    clrscr;
+    numeros := [2..255];
+    writeln('Calculadora de numeros primos');
+    writeln('Insira um valor maximo entre 2 e 255');
+    writeln('sera mostrado todos os primos ate esse valor');
+    repeat
+        readln(num);
+        if num in numeros = false then
+            writeln('Valor inserido invalido, insira outro');
+    until num in numeros;
+    val_primos := testa_primo2(num);
+    writeln('Valores primos: ');
+    for i := 1 to max do
+        if i in val_primos then
+            write(i, ' ');
+    readln;
+end.
