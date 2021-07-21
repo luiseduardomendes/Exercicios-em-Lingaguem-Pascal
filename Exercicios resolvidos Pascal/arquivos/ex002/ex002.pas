bea53cbc927ae8c8ac3138d530d7d8993a11d920
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-07-21 15:22:17 -0300

    create ex003.pas

--- Exercicios resolvidos Pascal/arquivos/ex002/ex002.pas
+++ Exercicios resolvidos Pascal/arquivos/ex002/ex002.pas
@@ -1,23 +1,40 @@
 program ex044;
-
 var
+    arquivo, arquivo_novo : text;
     lido : string;
-	arquivo : text;
+    caract : char;
 procedure abrir_arquivo;
 begin
-  	assign(arquivo, 'texto_ex044.txt');
+    assign(arquivo, 'texto_ex044.txt');
     rewrite(arquivo);
 end;
 
 begin
-  	abrir_arquivo;
-    writeln('Digite uma frase: ');
+    writeln('Digite um texto: ');
     readln(lido);
-	while lido <> '0' do
-	begin
-    	writeln(arquivo, lido);
-		writeln('Digite outra frase ou 0 para parar: ');
-        readln(lido);
-	end;
+    abrir_arquivo;
+    while lido <> '0' do
+    begin
+         writeln(arquivo, lido);
+         writeln('Digite outro texto [0 para parar]: ');
+         readln(lido);
+    end;
+    close(arquivo);
+    assign(arquivo_novo, 'texto_ex044.2.txt');
+    rewrite(arquivo_novo);
+    reset(arquivo);
+    while not eof(arquivo) do
+    begin
+        read(arquivo, caract);
+        if caract = '#' then
+            writeln(arquivo_novo, '')
+        else
+            write(arquivo_novo, caract);
+    end;
+    close(arquivo);
+    erase(arquivo);
+    close(arquivo_novo);
+    rename(arquivo_novo, 'texto_ex044.txt');
 
+    readln;
 end.
